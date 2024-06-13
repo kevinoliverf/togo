6c98addd0dfd31e766a73388a64fc904fffc19f3
Kevin Oliver Fernandez <kevinoliverf@student.unimelb.edu.au>
2024-06-13 22:17:28 +1000

    Updated to use protovalidate

--- cmd/proto_handler/task_helper.go
+++ cmd/proto_handler/task_helper.go
@@ -3,25 +3,35 @@ package main
 import (
 	"context"
 	"log"
+	"strings"
 
+	"github.com/bufbuild/protovalidate-go"
 	"github.com/kozloz/togo/internal/errors"
 	"github.com/kozloz/togo/internal/genproto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 )
 
+var v *protovalidate.Validator
+
+func init() {
+	var err error
+	v, err = protovalidate.New()
+	if err != nil {
+		log.Fatalf("failed to initialize protovalidate.Validator: %v", err)
+	}
+}
+
 // Validate the protobuf request
 func Validate(ctx context.Context, createTaskReq *genproto.CreateTaskRequest) error {
 	log.Printf("Validating request: %v", createTaskReq)
-	if createTaskReq.GetUserID() == 0 {
-		log.Printf("Error: Invalid UserID provided")
+	err := v.Validate(createTaskReq)
+	if err != nil {
+		cleanedErr := strings.ReplaceAll(err.Error(), "\n", " ")
+		log.Printf("Error: request validation failed: %s", cleanedErr)
+
 		_ = grpc.SetHeader(ctx, metadata.Pairs("x-http-code", "400"))
-		return errors.InvalidUserID
-	}
-	if createTaskReq.GetName() == "" {
-		log.Printf("Error: Empty task name provided")
-		_ = grpc.SetHeader(ctx, metadata.Pairs("x-http-code", "400"))
-		return errors.InvalidTaskName
+		return errors.InternalError
 	}
 	return nil
 }
--- go.mod
+++ go.mod
@@ -3,18 +3,21 @@ module github.com/kozloz/togo
 go 1.20
 
 require (
-	github.com/aws/aws-sdk-go v1.53.19
+	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.34.2-20240508200655-46a4cf4ba109.2
+	github.com/bufbuild/protovalidate-go v0.6.2
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0
 	google.golang.org/genproto/googleapis/api v0.0.0-20240604185151-ef581f913117
 	google.golang.org/grpc v1.64.0
-	google.golang.org/protobuf v1.34.1
+	google.golang.org/protobuf v1.34.2
 )
-
-require github.com/jmespath/go-jmespath v0.4.0 // indirect
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/google/cel-go v0.20.1 // indirect
+	github.com/stoewer/go-strcase v1.3.0 // indirect
+	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
 	golang.org/x/net v0.23.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
